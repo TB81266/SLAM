e39f0d3a5d4f97b1111aae395a8ca58fc23f637f
TB81266 <xxxxshang@163.com>
2020-06-16 19:25:12 +0800

    first commit

---
+++ slam/Gauss-Newton-Code/cmake-build-debug/CMakeFiles/GaussNewton.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GaussNewton.dir/Gauss-Newton-Code.cpp.o"
+  "GaussNewton.pdb"
+  "GaussNewton"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GaussNewton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
